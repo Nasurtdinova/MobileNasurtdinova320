6dc4f95b41e8ce9393f71c93809384a7b2889cab
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-02-13 20:18:55 +0300

    Rename folder name sql

---
+++ TestNasurtdinova320/TestNasurtdinova320/SQL_Lite/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace TestNasurtdinova320.SQL_Lite
+{
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        [Unique]
+        public string Email { get; set; }
+    }
+}
